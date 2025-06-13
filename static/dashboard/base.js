be3edf4bfdacb887e6be0127e43f200c5b32f866
Luis Arturo Rosario <luis@cpal.org>
2025-06-13 15:51:34 -0500

    :sparkles: add offcanvas component and switch to `django-bootstrap5` package

--- static/dashboard/base.js
+++ static/dashboard/base.js
@@ -1,7 +1,22 @@
 /* global bootstrap */
 
+// Show modal on page load
 window.onload = function() {
     "use strict";
     var myModal = new bootstrap.Modal(document.getElementById('intro-modal'));
     myModal.show();
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    const offcanvasElements = document.querySelectorAll('.offcanvas');
+
+    offcanvasElements.forEach(function(offcanvasEl) {
+        offcanvasEl.addEventListener('show.bs.offcanvas', function () {
+            document.body.classList.add('offcanvas-active');
+        });
+
+        offcanvasEl.addEventListener('hide.bs.offcanvas', function () {
+            document.body.classList.remove('offcanvas-active');
+        });
+    });
+});

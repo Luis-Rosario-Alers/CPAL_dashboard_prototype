3f3657edda8a35b5dc8f42b8623058bdcbe726e2
Luis Arturo Rosario <luis@cpal.org>
2025-06-14 15:11:10 -0500

    :sparkles: add `pre-commit` hooks

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    return render(request, "dashboard/home.html")
